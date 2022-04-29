b9903e28c1566e2672e877e6fca2913c2939414e
AndreiMazilu3741 <100567714+AndreiMazilu3741@users.noreply.github.com>
2022-04-29 16:51:46 +0100

    Add files via upload

--- Project4_dictionaries/JS/main.js
+++ Project4_dictionaries/JS/main.js
@@ -3,6 +3,8 @@ function dictionary_Function() {
         John_Smith : "Manager",
         Marry_Garcia : "Secretary",
         George_Michaelson : "Driver",
+        William_Moore : "Security",
     }
-    document.getElementById("Dictionary").innerHTML = employees_jobs.George_Michaelson;
+    delete employees_jobs.George_Michaelson
+    document.getElementById("Dictionary").innerHTML = employees_jobs.George_Michaelson; // this will return undefined due to delete operator
 }
