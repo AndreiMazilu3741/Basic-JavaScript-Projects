1fe6cb0308184f2edf4f52db2ef6609369f70f8a
AndreiMazilu3741 <100567714+AndreiMazilu3741@users.noreply.github.com>
2022-04-29 16:39:41 +0100

    Add files via upload

---
+++ Project4_dictionaries/JS/main.js
@@ -0,0 +1,8 @@
+function dictionary_Function() {
+    var employees_jobs = {
+        John_Smith : "Manager",
+        Marry_Garcia : "Secretary",
+        George_Michaelson : "Driver",
+    }
+    document.getElementById("Dictionary").innerHTML = employees_jobs.George_Michaelson;
+}
