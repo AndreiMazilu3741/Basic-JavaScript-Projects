297c0069aa64c826eba3ef6cc04d54c6c54dcef4
AndreiMazilu3741 <100567714+AndreiMazilu3741@users.noreply.github.com>
2022-05-03 23:00:50 +0100

    Add files via upload

--- Project7_scope_time_function/JS/main.js
+++ Project7_scope_time_function/JS/main.js
@@ -1,18 +1,21 @@
+var day = new Date().getDay(); // global variables
+var hour = new Date().getHours() 
 function wake_Up() {
-    if ((new Date().getDay() >= 3) && (new Date().getDay() <= 5)) { // days off are Wednesday to Friday
+    if ((day >= 3) && (day <= 5)) { // days off are Wednesday to Friday
         document.getElementById("Waking_Time").innerHTML = "Sleep well, today you are off<br>";
     }
-    else if (((new Date().getDay() == 6) || (new Date().getDay() <= 2)) && (new Date().getHours() < 15)) { // working days are Saturday to Tuesday, waking hour 15
+    else if (((day == 6) || (day <= 2)) && (hour < 15)) { // working days are Saturday to Tuesday, waking hour 15
         document.getElementById("Waking_Time").innerHTML = "Continue sleeping. You still have few hours left to sleep<br>";
     }
     else {
         document.getElementById("Waking_Time").innerHTML = "Wake up! It's time to go to work<br>";
     }
 }
-var num1 = document.getElementById("First_Number").value;
-var num2 = document.getElementById("Second_Number").value; // the scope of num1 and num2 si global
+
 function compute_Average() {
-    var result = (num1 + num2)/2; // the scope of result is local
+    var num1 = Number(document.getElementById("First_Number").value); // use of local variable
+    var num2 = Number(document.getElementById("Second_Number").value);
+    var result = (num1 + num2)/2; 
     document.getElementById("Result").innerHTML = "The average of the numbers " + num1 + "and " + num2 + "is " + result;
 }
 
